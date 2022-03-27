b092ccfadc349642d20d25bfbdcc14cfc1d95c00
FelixDryselius <felix.dryselius@gmail.com>
2022-03-27 22:35:38 +0200

    Removed uneccessary code for locality python scripts.

--- analysis_felix_locality.py
+++ analysis_felix_locality.py
@@ -7,11 +7,11 @@ start_time = time.time()
 
 spark = SparkSession.builder.appName("jan_small").master("spark://master:7077")\
     .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1')\
-    .config("spark.executor.memory", "2560m")\
+    .config("spark.executor.memory", "2g")\
     .config("spark.mongodb.input.database", "RedditComments")\
     .config("spark.mongodb.input.collection", "jan_2010")\
     .config("spark.mongodb.input.uri", "mongodb://192.168.2.14:27018, 192.168.2.129:27018/?readPreference=nearest")\
-    .config("spark.mongodb.input.localThreshold",10)\
+    .config("spark.mongodb.input.localThreshold",5)\
     .config("spark.mongodb.input.partitioner" ,"MongoShardedPartitioner") \
     .config("spark.mongodb.input.partitionerOptions.shardkey", "created_utc") \
     .getOrCreate()
@@ -20,59 +20,24 @@ sc = spark.sparkContext
 
 sc.setLogLevel("ERROR")
 
-#.option("query", "SELECT * FROM body")
 df = spark.read.format("mongo").option("query", "SELECT * FROM body").load()
 #df.show()
 
 def getWordCount(df, word):
 	dfFiltered = df.filter(df.body.rlike("(?i:"+word+")"))
-	#dfSplitted = dfFiltered.filter(lambda body: isBodyContainsWord(body,word))
 	return dfFiltered.count()
 
 def printWordsAndCounts(df, words):
     for word in words:
         print(word+"="+str(getWordCount(df, word)))
 
-words=['Russia', 'Russo', 'Ukraine', 'Ukrainian', 'Donbass']
+words=['Russia', 'Russo', 'Ukraine', 'Ukrainian']
 
-
-def filterDataFrameByWords(df, words):
-    found = False
-    for word in words:
-        if df.body.rlike("(?i:"+word+")"):
-            return True
-    return found
 
 printWordsAndCounts(df,words)
-
-def getConcanatedWords(words):
-    concan = ''
-    for word in words:
-        if(concan == ''):
-            concan += word
-        else:
-            concan += '|' + word
-    return concan
-
-#dfFiltered = df.filter(df.body.rlike("(?i:"+getConcanatedWords(words)+")"))
-#dfFiltered.show(truncate=False)
-
-#words2 = ['war', 'propaganda', 'manipulation', 'occupy', 'occupation', 'crisis', 'energy security', 'invasion', 'conflict', 'invade', 'invading', 'invasion', 'hostile', 'aggression', 'crimea', 'covert-ops', 'information war']
-#printWordsAndCounts(dfFiltered,words2)
 
 spark.stop()
 stop_time = time.time()
 time_execution = stop_time - start_time
 
 print("Time execution:" + str(time_execution )+ " sec")
-
-
-def isBodyContainsWord(body, word):
-	found = False
-	splits = body.split()
-	for split in splits:
-		if split.lower() == word.lower():
-			return True
-	return found
-
-
